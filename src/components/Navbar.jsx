0e5b244824b0aa0ff1b9e294a02a0fb801007ba7
Michał Chalimoniuk <michalchalimoniuk29@gmail.com>
2018-01-22 13:42:36 +0100

    Poprawka a => Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = ({ props }) => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="#">Unsplash Browser</a>
+      <Link className="navbar-brand" to="/">Unsplash Browser</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
