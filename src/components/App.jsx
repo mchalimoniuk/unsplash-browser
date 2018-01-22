3b44190775bf97df0a2d6a40d7c2240ea6e2ca7a
Michał Chalimoniuk <michalchalimoniuk29@gmail.com>
2018-01-22 13:39:22 +0100

    Dodanie bootstrapa

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Navbar from './Navbar';
 import PhotoRoute from '../routes/photo';
 import SectionRoute from '../routes/section';
 import SectionsRoute from '../routes/sections';
 
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import '../assets/css/bootstrap.min.css';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Navbar />
         <Switch>
           <Route exact path='/' component ={SectionsRoute} />
           <Route path='/sections/:id' component ={SectionRoute} />
