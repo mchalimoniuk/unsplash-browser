1f849535385fdcc1367743d0adebb5d9abd02431
Michał Chalimoniuk <michalchalimoniuk29@gmail.com>
2018-01-22 12:39:16 +0100

    Dodanie Reduxa i Routera

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import reduxThunk from 'redux-thunk';
+
 import './index.css';
+import reducers from './reducers';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
-  <App />
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
   , document.getElementById('root'));
 registerServiceWorker();
